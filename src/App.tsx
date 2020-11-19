bce7f5a2e0cdf81b8d5a86298486b38d8f0231a4
zhaoziwei <1728434772@qq.com>
2020-11-20 00:30:58 +0800

    feat Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import React from 'react'
+import Button, { ButtonSize, ButtonType } from '@components/Button/button'
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+    <div className='App' style={{ padding: 24, display: 'flex', justifyContent: 'space-between', width: 1000 }}>
+      <Button disabled>hello</Button>
+      <Button
+        onClick={e => {
+          alert(123)
+        }}>
+        normal
+      </Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+        Large Primary
+      </Button>
+      <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>
+        small Danger
+      </Button>
+      <Button btnType={ButtonType.Link} href={'https://www.baidu.com'} target={'_blank'}>
+        Baidu Link
+      </Button>
+      <Button disabled btnType={ButtonType.Link} href={'https://www.baidu.com'}>
+        Disable Link
+      </Button>
     </div>
   )
 }
